2d9546c9f0362995c9287413fcc072feb2dd0fcf
Rafael <rafahidalgo.83@gmail.com>
2018-02-14 11:40:53 +0100

    Implementación del método discoverPeople

--- VideoLibrary/Network/MovieDatabaseRepository.swift
+++ VideoLibrary/Network/MovieDatabaseRepository.swift
@@ -27,8 +27,12 @@ struct MovieDatabaseRepository: MovieRepository {
         
         Alamofire.request("\(self.apiUrl)\("discover/movie")",
             method: .get,
-            parameters: ["api_key":self.apiKey, "language":"es-ES",
-                         "sort_by":"popularity.desc", "include_adult":"false", "include_video":"false", "page":"1"])
+            parameters: ["api_key":self.apiKey,
+                         "language":"es-ES",
+                         "sort_by":"popularity.desc",
+                         "include_adult":"false",
+                         "include_video":"false",
+                         "page":"1"])
             .responseJSON(completionHandler: {response in
                 if let data = response.result.value {
                     let json = JSON(data)
@@ -41,5 +45,35 @@ struct MovieDatabaseRepository: MovieRepository {
         )
         
     }
+    
+    
+    func discoverPeople(completionHandler: @escaping (JSON?, Error?) -> ()) {
+        
+        Alamofire.request("\(self.apiUrl)person/popular",
+            method: .get,
+            parameters: ["api_key": self.apiKey,
+                         "language": "es-ES",
+                         "page": "1"])
+            .responseJSON { response in
+                if let data = response.result.value {
+                    let json = JSON(data)
+                    completionHandler(json, nil)
+                    return
+                }
+                let error = response.result.error
+                completionHandler(nil, error)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
