89aeb2b7bd60b9c9d545ef56163700b1649a849e
Rafael <rafahidalgo.83@gmail.com>
2018-02-14 11:22:53 +0100

    Se crea la vista de actores y el controlador de la celda de la colección

---
+++ VideoLibrary/Controllers/PeopleViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PeopleViewCell.swift
+//  VideoLibrary
+//
+//  Created by MIMO on 14/2/18.
+//  Copyright © 2018 MIMO. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var peopleName: UIImageView!
+    @IBOutlet weak var peopleImage: UILabel!    
+    
+}
