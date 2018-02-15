aae2c7abbe79062af7be2a1f9097fb8723aa7d44
Rafael <rafahidalgo.83@gmail.com>
2018-02-15 22:30:36 +0100

    Modificación de pantalla People
    *Se ponen las fotos de los actores con diseño de card view

--- VideoLibrary/Controllers/PeopleViewController.swift
+++ VideoLibrary/Controllers/PeopleViewController.swift
@@ -51,11 +51,21 @@ class PeopleViewController: UIViewController, UICollectionViewDataSource, UIColl
         cell.actorName.text = self.people[indexPath.row].name
         if let imageURL = self.people[indexPath.row].photoURL {
             cell.actorImage.af_setImage(withURL: URL(string: "https://image.tmdb.org/t/p/w500\(imageURL)")!)
-            cell.actorImage.clipsToBounds = true
-            cell.actorImage.layer.cornerRadius = cell.actorImage.layer.frame.height / 2
         } else {
             //TODO si no hay foto
         }
+        
+        cell.contentView.layer.cornerRadius = 10.0
+        cell.actorImage.layer.cornerRadius = 10.0 //TODO Quitar radio de la parte inferior
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
         
         return cell
         
