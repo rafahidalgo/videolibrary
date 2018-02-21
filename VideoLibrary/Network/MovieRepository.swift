4c41183911d3c38d6da7c1b901fd224fca484b14
Rafael <rafahidalgo.83@gmail.com>
2018-02-21 18:09:32 +0100

    Función para obtener los detalles de una persona
    Se obtienen los detalles de una persona a partir de una id dada.
    
    *Se define la función en el protocolo MovieRepository
    *se implementa la función en la estructura MovieDatabaseRepository

--- VideoLibrary/Network/MovieRepository.swift
+++ VideoLibrary/Network/MovieRepository.swift
@@ -17,19 +17,24 @@ protocol MovieRepository {
     
     init()
     
+    //Movies
     func discoverMovies(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getPopularMovies(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getTopRatedMovies(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func moviesReleaseDateAsc(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     
+    //TV Shows
     func discoverTVShows(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getPopularTVShows(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getTopRatedTVShows(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getOnAirTVShows(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     
-    func getPosterImage(poster: String, view: UIImageView)
+    //People
     func discoverPeople(page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
     func getPerson(name: String, page: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
-
+    func getPersonDetail(id: Int, completionHandler: @escaping (JSON?, NSError?) -> ())
+    
+    //Resources
+    func getPosterImage(poster: String, view: UIImageView)
 
 }
