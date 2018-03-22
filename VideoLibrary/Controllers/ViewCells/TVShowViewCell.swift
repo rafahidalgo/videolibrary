93648b285c209c27f795cdc3b48b5ee5cff5658e
Rafael <rafahidalgo.83@gmail.com>
2018-03-22 13:39:15 +0100

    Se da formato variable a las celdas de Movies y TVShows

--- VideoLibrary/Controllers/ViewCells/TVShowViewCell.swift
+++ VideoLibrary/Controllers/ViewCells/TVShowViewCell.swift
@@ -16,4 +16,31 @@ class TVShowViewCell: UICollectionViewCell {
     @IBOutlet weak var showAirDate: UILabel!
     @IBOutlet weak var voteAverage: UICircularProgressRingView!
     @IBOutlet weak var favoriteButton: UIButton!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.formatCell()
+    }
+    
+}
+
+//Formato de las celdas
+extension TVShowViewCell {
+    func formatCell() {
+        
+        //Tamaño del título
+        let nameFont = self.showName.font.withSize(self.showName.bounds.height/2)
+        self.showName.font = nameFont
+        
+        //Tamaño de la fecha de estreno
+        let dateFont = self.showAirDate.font.withSize(self.showAirDate.bounds.height/2.5)
+        self.showAirDate.font = dateFont
+        
+        //Tamaño del círculo de votación
+        let heigthAverageView = self.voteAverage.bounds.height
+        let ringFont = self.voteAverage.font.withSize(heigthAverageView/3)
+        self.voteAverage.font = ringFont
+        self.voteAverage.innerRingWidth = heigthAverageView / 8
+        self.voteAverage.outerRingWidth = heigthAverageView / 6
+    }
 }
